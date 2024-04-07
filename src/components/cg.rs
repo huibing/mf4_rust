8d62ba8e7fd9abb83df8f52113463ab19c7c01c9
bing.liu4 <bing.liu4@nio.com>
2024-04-07 18:36:11 +0800

    move test code to component.rs to avoid bufreader sync confilict problem.

--- src/components/cg.rs
+++ src/components/cg.rs
@@ -95,38 +95,3 @@ pub mod channelgroup {
 }
 
 
-#[cfg(test)]
-pub mod channelgroup_test {
-    use crate::components::cg::channelgroup::*;
-    use rust_embed::RustEmbed;
-    use std::io::BufReader;
-    use std::fs::File;
-    use std::io::Write;
-    use std::sync::Mutex;
-    use rstest::*;
-
-    #[derive(RustEmbed)]
-    #[folder = "test/"]
-    #[prefix = "test/"]
-    struct Asset;
-
-    #[fixture]
-    #[once]
-    fn buffer() -> Mutex<BufReader<File>> {
-        let file_data = Asset::get("test/1.mf4").unwrap();
-        let mut new_file = File::create("temp.mf4").unwrap();
-        new_file.write(file_data.data.as_ref()).unwrap();
-        let file = File::open("temp.mf4").unwrap();
-        let buf= BufReader::new(file);
-        Mutex::new(buf)
-    }
-
-    #[rstest]
-    fn test_new(buffer: &Mutex<BufReader<File>>) {
-        let offset = 0x6400;
-        let mut buf = buffer.lock().unwrap();
-        let cg: ChannelGroup = ChannelGroup::new(&mut buf, offset).unwrap();
-        println!("{:?}", cg);
-    }
-    
-}
